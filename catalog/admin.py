40406482394934457ff4f3fb850d5cc987d0048b
Nathaniel <nathanielabebe32@gmail.com>
2025-10-29 21:55:40 +0300

    admin site built

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Book, BookInstance, Language, Genre
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
